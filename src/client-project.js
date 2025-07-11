642d68f1162614889a44605c5914ec30c9d271fb
Jacob Casas <95025463+jacobcasas@users.noreply.github.com>
2025-07-11 14:11:47 -0700

    stupid dumb stuff for netlify

--- src/client-project.js
+++ src/client-project.js
@@ -1,4 +1,4 @@
-import { clients } from "./client-list";
+import { clients } from "./client-list.js";
 
 document.addEventListener("DOMContentLoaded", () => {
     const params = new URLSearchParams(window.location.search);
